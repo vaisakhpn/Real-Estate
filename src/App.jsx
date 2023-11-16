b71f1f4bfc607677e04ee78eef353c32bca57305
Vaisakh <vaisakhpn78@gmail.com>
2023-11-17 00:46:51 +0530

    Added logo and searchbar and navigation links to header

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import About from "./pages/About"
 import Profile from "./pages/Profile"
 import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
+import Header from "./components/Header"
 
 export default function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/about" element={<About/>} />
