f840bb9cbf55474c0740046e3bbc34cf00c4e6dd
Vaisakh <vaisakhpn78@gmail.com>
2023-11-18 13:00:31 +0530

    created midleware

--- api/index.js
+++ api/index.js
@@ -24,3 +24,17 @@ app.listen(3000, () => {
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+
+app.use((err,rq,res,next)=>{
+
+  const statusCode=err.statusCode || 500;
+  const message=err.message || "internal Server Error";
+
+  return res.status(statusCode).json({
+
+    success:false,
+    statusCode,
+    message,
+  });
+
+});
