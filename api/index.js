db0618b8b3c0caa363c1e7dd5874c69808be6480
Vaisakh <vaisakhpn78@gmail.com>
2023-11-22 08:28:55 +0530

    added create list api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import listingRouter from './routes/listing.route.js'
 import userRouter  from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js"
 import cookieParser from "cookie-parser";
@@ -26,6 +27,7 @@ app.listen(3000, () => {
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+app.use("/api/listing",listingRouter);
 
 app.use((err,req,res,next)=>{
 
