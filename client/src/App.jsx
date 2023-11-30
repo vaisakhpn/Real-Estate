db3305c65bc5715cdf401a61dd13f47285c85ebf
Vaisakh <vaisakhpn78@gmail.com>
2023-12-01 00:55:02 +0530

    complete update listing function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from "./pages/Signup";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/create-listing" element={<CreateListing/>}/>
+        <Route path="/update-listing/:listingId" element={<UpdateListing/>}/>
       </Routes>
     </BrowserRouter>
   );
