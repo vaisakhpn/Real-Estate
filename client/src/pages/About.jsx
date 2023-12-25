3fbec6a919b1bfabed3b08420edeb7a1388693a1
Vaisakh <vaisakhpn78@gmail.com>
2023-12-26 01:15:53 +0530

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="p-7 max-w-6xl mx-auto my-11">
+      <h1 className="text-3xl font-bold">About Vyshak Estate</h1>
+      <p className="p-3 text-lg text-slate-600">
+        "Welcome to Vyshaks Estate, your trusted destination for finding the
+        perfect home! Explore our comprehensive database of available
+        properties, tailored to match your unique preferences and requirements.
+        Whether you're in search of a cozy apartment, a spacious family house,
+        or a luxurious estate, our user-friendly interface and advanced search
+        tools make the process of discovering your dream home effortless.
+      </p>
+      <p className="p-3 text-lg text-slate-600">
+        Browse through a wide range of listings, including detailed
+        descriptions, high-quality images, and virtual tours that provide an
+        immersive experience, allowing you to envision yourself in your future
+        home. Our intuitive search filters enable you to refine your search by
+        location, price range, property type, amenities, and more, ensuring that
+        you find precisely what you're looking for.
+      </p>
+      <p className="p-3 text-lg text-slate-600">
+        At Vyshaks Estate, we prioritize your satisfaction and convenience. Our
+        team of experienced real estate professionals is committed to guiding
+        you through every step of the home-buying process, offering expert
+        advice, answering your queries, and facilitating smooth transactions.
+      </p>
+    </div>
+  );
 }
